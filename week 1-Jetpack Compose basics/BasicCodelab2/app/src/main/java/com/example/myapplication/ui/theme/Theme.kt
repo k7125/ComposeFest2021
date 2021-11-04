9c018dde6f09718ebe608585c5ffa1a69bd819f8
김영배 <youngbae.kim@daekyo.co.kr>
2021-11-04 13:08:35 +0900

    Jetpack Compose basics 진행 완료

---
+++ week 1-Jetpack Compose basics/BasicCodelab2/app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+)
+
+private val LightColorPalette = lightColors(
+    surface = Blue,
+    onSurface = androidx.compose.ui.graphics.Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
+)
+
+@Composable
+fun MyApplicationTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
